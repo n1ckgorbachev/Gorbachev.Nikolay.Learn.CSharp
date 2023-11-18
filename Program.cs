73c8980b8bdc6b4dc21bf3c9627a8977badce6ce
Nick Gorbachev <n1kko.gorbachevv@gmail.com>
2023-11-18 16:36:59 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gorbachev.Nikolay.Learn.CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
